35b6f3329384f3ff3f4e4c57f8a89456be43dfb7
Dannenberg <mdannenberg@uiowa.edu>
2020-01-13 11:08:15 -0600

    Initial commit

---
+++ eco_model.r
@@ -0,0 +1,412 @@
+## Functions for ecological model
+
+date2doy <- function(yr, mo, dy){
+  yr <- as.character(yr)
+  mo <- as.character(mo)
+  dy <- as.character(dy)
+  
+  dt <- paste(c(yr,'-',mo,'-',dy), collapse='')
+  doy <- strftime(dt, format='%j')
+  return(doy)
+}
+
+define_global_variables <- function(){
+  
+  # Site constants
+  PAR2SWIR <<- 0.43 # PAR to shortwave radiation ratio
+  STD_MERAD <<- -75.0 # Time zone longitude
+  LATI <<- 35.9736 # Site latitude (CHANGE TO MMS)
+  LONGI <<- -79.1004 # Site longitude (CHANGE TO MMS)
+  hs <<- 20.0 # Screen height
+  h2 <<- 16.2 # Default canopy height
+  PA <<- 101325.0 # Air pressure in Pa
+  CP <<- 1010.0 # Specific heat of air (J kg-1 K-1)
+  CA <<- 410.0 # Atmospheric CO2 concentration (ppm)
+  
+  # Physical constants
+  SO <<- 1367.0 # Solar constant (W m-2)
+  
+  # Leaf constants
+  alpha_par <<- 0.80
+  alpha_nir <<- 0.20
+  rho_s <<- 0.10
+  x <<- 1.0 # Leaf angle distribution parameter
+  omega <<- 1.0 # Clumping index
+  m <<- 8.0 # Ball-Berry model slope
+  g1_blayer <<- 0.005 # Leaf boundary layer conductance in m/s
+  g0 <<- 0.010 # Parameter in Leuning's gs function in mol m-2 s-1
+  
+  # Soil constants
+  Ksat <<- 0.6 # Saturated conductance (m day-1)
+  wilting_p <<- 0.08 # Wilting poit soil water content (unitless)
+  porosity <<- 0.54 # Porosity at depth zero (unitless)
+  p1 <<- 0.478 # Air entry pressure in meters of water
+  p2 <<- 0.186 # Pore size index (unitless)
+  p_0 <<- 4000.0 # Porosity decay in m-1
+  m_z <<- 0.24 # Saturated conductivity decay parameter (in m-1)
+  soil_depth <<- 0.325 # Soil depth in rooting zone
+  
+}
+
+canopy_aerodynamic_resistance <- function(u_h, h, h_o){
+  h_u <- 2.0
+  cn <- 0.002 # wind attenuation coefficient
+  
+  # Compute zero plane displacement
+  d_o <- 0.7 * h_o
+  
+  # Compute the roughness length
+  zo_o <- 0.1 * h_o
+  u_o <- compute_toc_wind(u_h, h, h_o)
+  u_m <- (u_o * exp(cn * (0.5 * h_o)/h_o - 1))
+  u_m <- sapply(u_m, function(x) max(x,0))
+  
+  if(h<d_o) h <- d_o + 0.0001
+  
+  ra <- (log((h_o-d_o)/zo_o)/0.41)^2 / u_m
+  ra[u_m==0] <- 9999999
+  
+  return(ra)
+}
+
+canopy_bwb_stomatal_conductance <- function(psn, Tair, a1, vpda){
+  svp <- 610.7 * exp(17.38 * Tair / (239.0+Tair))
+  rh <- 1.0 - vpda/svp
+  gc <- g0 + a1 * psn * rh/CA
+  
+  # convert mol m-2 s-1 to m s-1
+  gc <- gc * (22.4 / 1000.0)
+  
+  # convert gc (Co2) to gs (H2O)
+  gs <- 1.56 * gc
+  
+  return(data.frame(gc=gc, gs=gs))
+}
+
+compute_bwb_farq_psn <- function(psnp, apar_wm2, Tair, vpda, fstheta, leaftype){
+  svp <- 610.7 * exp(17.38 * Tair / (239.0 + Tair))
+  rh <- 1.0 - vpda/svp
+  apar <- apar_wm2*4.55
+  Ca <- CA
+  
+  # calculate atmospheric O2 in Pa, assuming 21% O2 by volume
+  O2 <- psnp$O2
+  Ko <- psnp$Ko
+  
+  # Convert Pa to ppm
+  Kc <- psnp$Kc * 1e6/PA
+  gamma <- psnp$gamma * 1e6/PA
+  
+  ppe <- psnp$ppe
+  if(leaftype==1){
+    Vmax <- psnp$Vmax_sunlit
+    Rd <- psnp$Rd_sunlit
+  } else {
+    Vmax <- psnp$Vmax_shaded
+    Rd <- psnp$Rd_shaded
+  }
+  
+  Jmax <- 2.1 * Vmax
+  
+  g1 <- 0.01 + m * rh/Ca
+  K <- Kc * (1.0 + O2/Ko)
+  
+  # Calculate rubisco-limited assimilation rate
+  aa <- Ca*g1 + g1*K - 1.0
+  bb <- Ca*g0 + g0*K - Vmax*Ca*g1 + Vmax + Vmax*g1*gamma + Rd*Ca*g1 - Rd + Rd*g1*K
+  cc = -Vmax*Ca*g0 + Vmax*gamma*g0 + Rd*Ca*g0 + Rd*K*g0
+  
+  det <- bb*bb - 4.0*aa*cc
+  Av <- (-bb + sqrt(det)) / (2.0*aa)
+  Av[det<0] <- -1.0
+  
+  # Calculate RuBP regeneration limited assimilation rate
+  aa <- 0.7
+  bb <- -Jmax - (apar/ppe)
+  cc <- Jmax * apar/ppe
+  J <- (-bb - sqrt(bb*bb - 4*aa*cc)) / (2.0/aa)
+  
+  aa <- 4.5*Ca*g1 - 4.5 + 10.5*gamma*g1
+  bb <- 4.5*Ca*g0 + 10.5*gamma*g0 - J*g1*Ca + J + gamma*g1*J + 4.5*Rd*Ca*g1-4.5*Rd+10.5*gamma*g1*Rd
+  cc <- -J*Ca*g0+4.5*Rd*Ca*g0+10.5*gamma*g0*Rd
+  
+  det <- bb*bb - 4.0*aa*cc
+  Aj <- (-bb+sqrt(det))/(2.0*aa)
+  Aj[det<0] <- -1.0
+  
+  # Calculate net assimilation rate
+  A = pmin(Aj,Av)
+  A[Av == -1.0 & Aj == -1.0] <- 0.0
+  A[Av == -1.0 & Aj != -1.0] <- Aj[Av == -1.0 & Aj != -1.0]
+  A[Aj == -1.0 & Av != -1.0] <- Av[Aj == -1.0 & Av != -1.0]
+  
+  return(A)
+}
+
+compute_canopy_intercepted_shortwave_rad <- function(par_D, par_d, nir_D, nir_d, lai, Kb, Kd){
+  Q_sc_par <- par_D*(exp(-Kb*sqrt(alpha_par)*lai)-exp(-Kb*lai))/2.0
+  Q_d_par <- par_d*(1.0-exp(-Kd*sqrt(alpha_par)*lai))/(Kd*sqrt(alpha_par)*lai)
+  Q_par_soil <- par_D*exp(-Kb*sqrt(alpha_par)*lai)+par_d*exp(-Kd*sqrt(alpha_par)*lai)
+  Q_nir_soil <- nir_D*exp(-Kb*sqrt(alpha_nir)*lai)+nir_d*exp(-Kd*sqrt(alpha_nir)*lai)
+  
+  sunlit_apar <- alpha_par*(par_D*Kb+ Q_sc_par + Q_d_par)
+  shaded_apar <- alpha_par*(Q_sc_par + Q_d_par)
+  
+  Q_sc_nir <- nir_D*(exp(-Kb*sqrt(alpha_nir)*lai)-exp(-Kb*lai))/2.0
+  Q_d_nir <- par_d*(1.0-exp(-Kd*sqrt(alpha_par)*lai))/(Kd*sqrt(alpha_par)*lai)
+  
+  sunlit_anir <- alpha_nir*(nir_D*Kb+Q_sc_nir+Q_d_nir)
+  shaded_anir <- alpha_nir*(Q_sc_nir+Q_d_nir)
+  
+  rad <- data.frame(sunlit_apar=sunlit_apar,
+              shaded_apar=shaded_apar,
+              sunlit_anir=sunlit_anir,
+              shaded_anir=shaded_anir)
+  return(rad)
+}
+
+compute_canopy_net_rad <- function(rad_short, rad_long, toc_rad, sunlit_lai, shaded_lai, Kb, Kd){
+  
+  lai <- sunlit_lai + shaded_lai
+  
+  Rnet_sunlit <- rad_short$sunlit_apar + rad_short$sunlit_anir +
+    rad_long$floor * (1.0 - exp(-Kd*lai)) / (Kd*lai) +
+    rad_long$air * (1.0 - exp(-Kd*lai)) / (Kd*lai) +
+    -2.0 * rad_long$canopy * (1.0 - exp(-Kd*lai)) / (Kd*lai)
+  
+  Rnet_shaded <- rad_short$shaded_apar + rad_short$shaded_anir + 
+    rad_long$floor * (1.0 - exp(-Kd*lai)) / (Kd*lai) + 
+    rad_long$air * (1.0 - exp(-Kd*lai)) / (Kd*lai) + 
+    -2.0 * rad_long$canopy * (1.0 - exp(-Kd*lai)) / (Kd*lai)
+  
+  Rnet_floor <- (1.0 - rho_s) * (toc_rad$par_D * exp(-Kb*sqrt(alpha_par)*lai)+ 
+                                  toc_rad$par_d * exp(-Kd*sqrt(alpha_par)*lai)+ 
+                                  toc_rad$nir_D * exp(-Kb*sqrt(alpha_nir)*lai)+
+                                  toc_rad$nir_d * exp(-Kd*sqrt(alpha_nir)*lai))+
+                                  rad_long$air * exp(-Kd*lai)+ 
+                                  rad_long$canopy * (1.0-exp(-Kd*lai))-
+                                  rad_long$floor
+  
+  Rnet_canopy <- sunlit_lai*Rnet_sunlit + shaded_lai*Rnet_shaded
+  Rnet_stand <- Rnet_canopy + Rnet_floor
+  
+  out <- data.frame(Rnet_sunlit = Rnet_sunlit, 
+                    Rnet_shaded = Rnet_shaded, 
+                    Rnet_floor = Rnet_floor, 
+                    Rnet_canopy = Rnet_canopy, 
+                    Rnet_stand = Rnet_stand)
+  return(out)
+  
+}
+
+compute_longwave_rad <- function(Tair, Tsoil, vpd, cloud){
+  # Compute emitted longwave radiation from the canopy, floor, and air
+  
+  N = cloud
+  e_s = 610.7 * exp(17.38 * Tair / ( 239.0 + Tair))
+  e_a = (e_s-vpd)/100.0
+  
+  epsilon_air = 1.24 * (e_a/(Tair+273.13))^(1.0/7.0)
+  epsilon_air =(1.0-0.84*N)*epsilon_air + 0.84*N
+  
+  Ln_air = epsilon_air*(5.6704e-8) * (Tair+273.13)^4.0
+  Ln_canopy = 1.0*(5.6704e-8) * (Tair+273.13)^4.0
+  Ln_floor = 0.97*(5.6704e-8) * (Tsoil+273.13)^4.0
+  
+  emitted_rad <- data.frame(air=Ln_air, canopy=Ln_canopy, floor=Ln_floor, air_emissivity=epsilon_air)
+  return(emitted_rad)
+  
+}
+
+compute_potential_exfiltration <- function(Sr, depth_s, K_sat, Ksat_decay, psi_air_entry, pore_size_index, pore_decay, pore0){
+  # Estimate mean porosity
+  porosity_average <- pore0 * pore_decay * (1.0-exp(-1.0*depth_s/pore_decay))
+  
+  # Estimate mean saturated conductivity
+  if (Ksat_decay > 0){
+    Ksat_average <- Ksat_decay* Ksat *(1.0-exp(-1.0*depth_s/Ksat_decay))
+  } else Ksat_average <- Ksat
+  
+  S <- max(0,min(Sr,1))
+  
+  # Plug everything into the equation for max infiltration
+  potential_exfiltration <- S ^ ((1.0 / (2.0*pore_size_index))+2.0) * 
+                            sqrt((8.0 * porosity_average * 
+                            Ksat_average * psi_air_entry) / 
+                            (3.0 * (1.0 + 3.0 * pore_size_index) * 
+                            (1.0 + 4.0 * pore_size_index)))
+  
+  potential_exfiltration = min(0.001, potential_exfiltration)
+  pe_wm2 = potential_exfiltration * 1e6 * 597.3 * 4.18 / (24.0*3600.0)
+  return(pe_wm2)
+}
+
+compute_psn_parameters <- function(t, Kb, L_sunlit, L_shaded){
+  # Local static variables
+  Kc25 <- 404.0  # (ubar) MM const carboxylase, 25 deg C
+  q10Kc <- 2.1 # (DIM) Q_10 for kc
+  Ko25 <- 248.0 # (mbar) MM const oxygenase, 25 deg C
+  q10Ko <- 1.2 # (DIM) Q_10 for ko
+  act25 <- 3.6 # (umol/mgRubisco/min) Rubisco activity
+  q10act <- 2.4 # (DIM) Q_10 for Rubisco activity
+  Kn <- 0.52 # extinction coefficient for Nitrogen distribution
+  cica <- 0.67 # Ci to Ca ratio 
+  alpha <- 0.10 # quantum yield efficiency
+  
+  L <- L_sunlit + L_shaded
+  
+  # Calculate atmospheric O2 in Pa, assumes 21% O2 by volume
+  O2 <- 0.21 * PA
+  
+  # Correct kinetic constants for temperature, and do unit conversions
+  Ko <- Ko25 * q10Ko^((t-25.0)/10.0)
+  Ko <- Ko * 100.0 # mbar --> Pa
+  
+  Kc <- Kc25 * (1.8*q10Kc)^((t-25.0)/10.0) / q10Kc
+  Kc[t>15.0] <- Kc25 * q10Kc^((t[t>15.0]-25.0)/10.0)
+  act <- act25 * (1.8*q10act)^((t-25.0)/10.0) / q10act
+  act[t>15.0] <- act25 * q10act^((t[t>15.0]-25.0)/10.0)
+  
+  Kc <- Kc * 0.10 # ubar --> Pa
+  act <- act * 1e6/60.0 # umol/mg/min --> umol/kg/s
+  
+  # Calculate gamma (Pa), assumes Vomax/Vcmax = 0.21
+  gamma <- 0.5 * 0.21 * Kc * O2 / Ko
+  
+  # Calculate Vmax from leaf nitrogen data and Rubisco activity
+  Vmax25 <- 59.0 # umol/m2leaf/s at the top of the canopy (Lai et al, 2002, PCE, 25:1095-1119)
+  
+  Vmax25_canopy <- L * Vmax25 * (1.0 - exp(-L*Kn)) / (Kn*L)
+  Vmax25_sunlit <- L * Vmax25 * (1.0 - exp(-Kn-Kb*L)) / (Kn+Kb*L)
+  Vmax25_shaded <- (Vmax25_canopy - Vmax25_sunlit) / L_shaded
+  Vmax25_sunlit <- Vmax25_sunlit / L_sunlit
+  
+  Vmax_sunlit <- Vmax25_sunlit * exp(0.051*(t-25.0))/(1.0+exp(0.205*(t-41.0)))
+  Vmax_shaded <- Vmax25_shaded * exp(0.051*(t-25.0))/(1.0+exp(0.205*(t-41.0)))
+  
+  Rd_sunlit <- 0.015 * Vmax_sunlit
+  Rd_shaded <- 0.015 * Vmax_shaded
+  
+  ppe <- 1.0 / (alpha*(4.0*cica*CA+2.0*gamma)/(cica*CA-gamma))
+  
+  psn_para <- data.frame(gamma=gamma,
+                   Vmax_sunlit=Vmax_sunlit,
+                   Vmax_shaded=Vmax_shaded,
+                   Ko=Ko,
+                   Kc=Kc,
+                   O2=O2,
+                   Rd_sunlit=Rd_sunlit,
+                   Rd_shaded=Rd_shaded,
+                   ppe=ppe)
+  
+  return(psn_para)
+}
+
+compute_soil_surface_resistance <- function(theta){
+  gs <- 1.0 / (-83000.0 * theta + 16100.0)
+  gs[theta > 0.185] <- 0.001429
+  
+  rs <- 1.0 / gs
+  return(rs)
+}
+
+compute_sun_angles <- function(lat, lon, yr, mo, dt, hr, mi){
+  solar_time <- hr + mi/60.0 + (lon-STD_MERAD)/15.0
+  hangle <- (12.0-solar_time)*15.0*pi/180.0
+  
+  # Compute Julian day
+  jday <- as.numeric(mapply(date2doy, yr=yr, mo=mo, dy=dt))
+  
+  # Sun declination angle
+  declangle <- 23.45 * sin(2.0*pi*(284.0+jday)/365.0) * pi/180.0
+  
+  # sin(sun_elevation)
+  sinh <- sin(lat*pi/180.0)*sin(declangle)+cos(lat*pi/180.0)*cos(declangle)*cos(hangle)
+  
+  zenith <- acos(sinh)
+  azimuth <- asin(cos(declangle)*sin(hangle)/cos(pi/2.0-zenith))
+  
+  out <- data.frame(jday = jday, zenith = zenith, sun_decl = declangle, hourangle = hangle)
+  return(out)
+}
+
+compute_toc_down_rad <- function(par, sun_zenith, jday){
+  tau_t <- (par/4.55)*(1.0/PAR2SWIR)/(SO*(1.0+0.033*cos(2.0*pi*(jday-10.0)/365.0)))
+  
+  # Leuning (1995) approach
+  fd_l <- 1.0-2.0*(tau_t-0.3)
+  fd_l[tau_t > 0.7] <- 0.2
+  fd_l[tau_t < 0.3] <- 1.0
+  
+  # using Liu and Jordan (1960) and Leuning (1995) approach 
+  tau_d <- 0.384-0.416*tau_t
+  tau_D <- tau_t-tau_d
+  
+  
+    # new inovation: According to observations, at clear skies, 77% of the total
+    # radiation is direct (tau_D=0.77), and 23% of it is diffuse. Also according to 
+    # measurements, when tau_t=0.3 (i.e. tau_D=0.384-0.416*(0.3)=0.26), there is no direct light.  
+    # for clear skies, the very diffuse fraction of par is 1.4 time the total diffuse
+    # fraction. Assuming this fraction linearly decrease to 1.0 when tau_D=0.26. 
+    
+  
+  fdv2fd <- 1.0+0.8511*(tau_t-0.3)
+  fdv2fd[tau_t <= 0.3] <- 1.0
+  
+  fd_nir <- (2.0-fdv2fd)*fd_l
+  fd_par <- 2.0*fd_l-fd_nir
+  
+  rad <- data.frame(par_d = fd_par * par/4.55,
+                    par_D = par/4.55 - fd_par * par/4.55,
+                    nir_d = fd_nir * (par/4.55) * (0.57/0.43),
+                    nir_D = (par/4.55) * (0.57/0.43) - fd_nir * (par/4.55) * (0.57/0.43),
+                    cloud = max(1.0-tau_t/0.7, 0))
+  return(rad)
+  
+}
+
+compute_toc_wind <- function(u_h, h, z){
+  # Returns wind speed at height z in stratum
+  #   from RHESSys, which was from BGC 4.11.
+  
+  # Compute winds
+  d_o <- 0.63 * z
+  z_o <- 0.1 * z
+  
+  # Wind speed at toc log decrease from screen height to toc
+  u_z <- u_h * log((z-d_o)/z_o) / log((h-d_o)/z_o)
+  return(u_z)
+}
+
+penman_monteith <- function(Tair, Rnet, gs, ga, vpda){
+  # Calculate forest canopy ET using penman-monteith equation
+  
+    # dt:     delta t, a small temperature increase
+    # rho:    air density (kg/m2)
+    # lhvap:  latent heat of water (J/kg)
+    # s:      Slope of saturated vapor pressure - temp
+    # es:     Saturated vapor pressure at t2 in Pa
+    # ET:     Evapotranspiration in W/m2
+    # gamma:  Psychrometric constant
+    # z0:     Surface roughness
+    # d0:     Zero plane displacement
+  
+  # Density of air (rho) as a f'n of air temp
+  rho <- 1.292 - (0.00428*Tair)
+
+  # Latent heat of vaporization as a f'n of air temp
+  lhvap = 2.5023e6 - 2430.54*Tair
+  
+  # Saturation vapor pressures at Tair (Pa)
+  es <- 610.7 * exp(17.38 * Tair / ( 239.0 + Tair))
+  
+  # Slope of es-T curve at Tair (Pa/deg C) (from Campbell & Norman 1998)
+  s <- 17.38*239.0*es / (239.0+Tair)^2
+  
+  # Calculate gamma
+  gamma <- CP * PA / ( 0.622*lhvap )
+  
+  # Evaporation in W/m2
+  ET <- ((s*Rnet) + (rho*CP*vpda*ga)) / (gamma*(1.0 + ga/gs) +s)
+}
+
---
+++ main.r
@@ -0,0 +1,152 @@
+#########################################################
+#####  Main file: estimate radiation, conductance,  #####
+#####  photosynthesis, and evapotranspiration with  #####
+#####             process based model               #####
+#########################################################
+
+
+# Set working directory
+setwd('D:\\Code\\Eco_Model')
+
+# Load model functions
+source('eco_model.r')
+library(pracma)
+
+# Load data
+dat <- read.table('TdrTsoilWindLaiPrecParTaVpdMay2001-Daytime.txt')
+names(dat) <- c('Month', 'Day','Hour','Minute','Tdr','Tsoil','Wind','Lai','Prec','Par','Ta','Vpd')
+dat$Year <- 2001
+
+# Convert VPD from kPa to Pa
+dat$Vpd <- dat$Vpd*1000
+
+# Define global variables and model parameters
+define_global_variables()
+
+# Calculate solar zenith, elevation, azimuth, and declination angles
+sun <- compute_sun_angles(LATI, LONGI, dat$Year, dat$Month, dat$Day, dat$Hour, dat$Minute)
+
+# Calculate top of canopy diffuse/direct components of PAR and NIR
+rad <- compute_toc_down_rad(dat$Par, sun$zenith, sun$jday)
+
+# calculate sunlit and shaded components of LAI
+i <- seq(from=0, to=89, by=1)
+kb <- sqrt(x^2 + tan(i*pi/180)^2 / (x + 1.774 * (x+1.182)^-0.733))
+beer <- function(lai, kb, omega) exp(-kb * omega * lai) # Beer's law
+trans_D <- outer(dat$Lai, kb, beer, omega=omega) # apply function across all LAIs and angles
+trans_d <- apply(trans_D, 1, function(trans_D, i) trapz(i, 2*trans_D*sin(i*pi/180.0)*cos(i*pi/180.0)*(pi/180)), i=i) # integrate across all angles
+
+Kd <- -log(trans_d)/dat$Lai
+Kb <- omega * sqrt(x^2 + tan(sun$zenith)^2) / (x + 1.774*(x+1.182)^-0.733)
+
+sunlit_lai <- (1 - exp(-Kb*dat$Lai)) / Kb
+shaded_lai <- dat$Lai - sunlit_lai
+
+# Compute canopy shortwave radiation
+canopy_shortwave <- compute_canopy_intercepted_shortwave_rad(rad$par_D,
+                                                             rad$par_d, 
+                                                             rad$nir_D, 
+                                                             rad$nir_d, 
+                                                             dat$Lai, 
+                                                             Kb, 
+                                                             Kd)
+
+# Compute canopy longwave radiation
+canopy_longwave <- compute_longwave_rad(dat$Ta, dat$Tsoil, dat$Vpd, rad$cloud)
+
+# Compute net radiation
+canopy_net_rad <- compute_canopy_net_rad(canopy_shortwave, 
+                                         canopy_longwave,
+                                         rad, 
+                                         sunlit_lai, 
+                                         shaded_lai, 
+                                         Kb, 
+                                         Kd)
+
+# Estimate photosynthesis parameters
+psn_para <- compute_psn_parameters(dat$Ta, Kb, sunlit_lai, shaded_lai)
+
+# Estimate aerodynamic resistance and conductance
+ra <- canopy_aerodynamic_resistance(dat$Wind, hs, h2)
+ra[dat$Wind <= 0] <- 1.0 / g1_blayer
+ga <- 1/ra
+
+# Compute soil resistance
+rs_u <- compute_soil_surface_resistance(dat$Tdr)
+ra_u <- ra
+fsoil_theta <- 1.0
+
+# Calcuate photosynthesis and stomatal conductance for sunlit leaves
+sunlit <- 1;
+psn_sunlit <- compute_bwb_farq_psn(psn_para, 
+                                   canopy_shortwave$sunlit_apar,
+                                   dat$Ta, 
+                                   dat$Vpd, 
+                                   fsoil_theta, 
+                                   sunlit)
+
+out <- canopy_bwb_stomatal_conductance(psn_sunlit, dat$Ta, m, dat$Vpd)
+gs_sunlit <- out$gs
+gc_sunlit <- out$gc
+
+rs_sunlit <- 1/gs_sunlit
+rs_sunlit[gs_sunlit<=0] <- 1e6
+
+# Calculate photosynthesis and stomatal conductance for shaded leaves
+sunlit <- 0
+psn_shaded <- compute_bwb_farq_psn(psn_para, 
+                                   canopy_shortwave$shaded_apar,
+                                   dat$Ta, 
+                                   dat$Vpd, 
+                                   fsoil_theta, 
+                                   sunlit)
+
+out <- canopy_bwb_stomatal_conductance(psn_shaded, dat$Ta, m, dat$Vpd)
+gs_shaded <- out$gs
+gc_shaded <- out$gc
+
+rs_shaded <- 1/gs_shaded
+rs_shaded[gs_shaded<=0] <- 1e6
+
+# Calculate evapotranspiration using Penman-Monteith model
+ET_sunlit <- penman_monteith(dat$Ta, 
+                             canopy_net_rad$Rnet_sunlit, 
+                             gs_sunlit,
+                             ga, 
+                             dat$Vpd)
+
+ET_shaded <- penman_monteith(dat$Ta, 
+                             canopy_net_rad$Rnet_shaded, 
+                             gs_shaded,
+                             ga, 
+                             dat$Vpd)
+
+ET_understory <- penman_monteith(dat$Ta, 
+                                 canopy_net_rad$Rnet_floor, 
+                                 1/rs_u,
+                                 1/ra_u, 
+                                 dat$Vpd)
+
+S <- dat$Tdr / porosity
+
+Exfil_soil <- compute_potential_exfiltration(S, 
+                                             soil_depth, 
+                                             Ksat, 
+                                             m_z, 
+                                             p1,
+                                             p2, 
+                                             p_0, 
+                                             porosity)
+
+ET_understory <- pmin(Exfil_soil, ET_understory)
+
+# Add outputs to data frame
+dat$apar <- canopy_shortwave$sunlit_apar*sunlit_lai + 
+  canopy_shortwave$shaded_apar*shaded_lai
+dat$rnet <- canopy_net_rad$Rnet_stand
+dat$psn <- (psn_sunlit*sunlit_lai + psn_shaded*shaded_lai)*0.012
+dat$A <- psn_sunlit*sunlit_lai + psn_shaded*shaded_lai # um/m^2/s
+dat$gc <- 1000000*(gc_sunlit*sunlit_lai + gc_shaded*shaded_lai) / (22.4/1000.0) # um/m^2/s
+dat$et <- ET_sunlit*sunlit_lai + ET_shaded*shaded_lai + ET_understory
+
+
