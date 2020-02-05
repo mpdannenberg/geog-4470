8fb8226a7812eca847057673eeed44a653126d60
Dannenberg <mdannenberg@uiowa.edu>
2020-02-05 16:09:00 -0600

    Added new code

--- Lab1_Dannenberg.R
+++ Lab1_Dannenberg.R
@@ -1,5 +1,11 @@
 # Lab 1 practice session
 
+# When you use R for the first time in this class:
+  # Create a "Labs" folder on your computer
+  # Create "data" and "output" folders within the "Labs" folder
+  # In RStudio, create a new project (save as Labs.Rproj)
+  # Create a new script for each lab ("Lab1_<your last name here>.R")
+
 # 1) Defining variables
 lat <- 35.9736
 -79.1004 -> lon
@@ -32,6 +38,34 @@ plot(dat$Date, dat$Ta,
      ylab='Air temperature (degrees C)',
      frame=FALSE)
 lines(dat$Date, dat$Tsoil, col='red')
+legend(dat$Date[1], 10, legend=c('Air','Soil'), col=c('black','red'), lty=1)
+
+# 7) Plot a scatterplot
+plot(dat$Par, dat$Ta,
+     xlab='PAR',
+     ylab='Temperature',
+     frame=FALSE)
+
+# 8) Linear regression
+mdl <- lm(dat$Ta~dat$Par)
+mdl
+summary(mdl)
+abline(mdl)
+
+# 9) Write a function to convert celcius to kelvin
+c2k <- function(x){
+  y <- x + 273.15
+  return(y)
+}
+dat$Ta_kelvin <- c2k(dat$Ta)
+
+# 10) Indexing
+noon <- dat[dat$Hour==12 & dat$Minute==0, ]
+
+
+
+
+
 
 
 
